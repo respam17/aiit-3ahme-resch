3b172b2c0b52f14c39260421ec8aee7819d89741
respam17 <58695088+respam17@users.noreply.github.com>
2020-05-28 14:37:49 +0200

    Add files via upload

--- corona_Resch/src/klassendiagramme_koerper/Koerper.java
+++ corona_Resch/src/klassendiagramme_koerper/Koerper.java
@@ -9,6 +9,28 @@ package klassendiagramme_koerper;
  *
  * @author paul4
  */
-class Koerper {
-   
+public abstract class Koerper {
+    private double dichte;
+    public double DICHTE_EICHE = 670;
+    public double DICHTE_BUCHE = 690;
+
+    public Koerper(double dichte) {
+        this.dichte = dichte;
+    }
+
+    public double getDichte() {
+        return dichte;
+    }
+
+    public void setDichte(double dichte) {
+        this.dichte = dichte;
+    }
+    
+    public abstract double oberfläche();
+    
+    public abstract double volumen();
+    
+    public double masse() {
+        return volumen()*dichte;
+    }
 }
