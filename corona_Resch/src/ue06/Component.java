872441333cf384139b79831a56290b70a783ee36
respam17 <58695088+respam17@users.noreply.github.com>
2020-06-15 12:45:59 +0200

    Add files via upload

---
+++ corona_Resch/src/ue06/Component.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ue06;
+
+/**
+ *
+ * @author paul4
+ */
+ 
+public class Component {
+    
+    private final String id;
+    private final double value;
+    private double voltage;
+    private double current;
+
+    public Component(String id, double value) {
+        this.id = id;
+        this.value = value;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public double getVoltage() {
+        return voltage;
+    }
+
+    public double getCurrent() {
+        return current;
+    }
+
+    public void setVoltage(double voltage) {
+        this.voltage = voltage;
+    }
+
+    public void setCurrent(double current) {
+        this.current = current;
+    }
+
+    @Override
+    public String toString() {
+        return "Component{" + "id=" + id + ", value=" + value + ", voltage=" + voltage + ", current=" + current + '}';
+    }
+    
+    public abstract String unit ();
+
+    public abstract double energy ();
+}
---
+++ corona_Resch/src/ue06/TestSerialCircuit.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ue06;
+
+/**
+ *
+ * @author paul4
+ */
+public class TestSerialCircuit {
+
+ public static void main (String[] args) {
+
+ SerialCircuit sc = new SerialCircuit();
+ sc.setCurrent(1E-3); // 1mA
+
+ sc.add(new Resistor("R1", 1.1234)); // 1.1234Ω
+ sc.add(new Resistor("R2", 2200)); // 2.2kΩΩ
+ sc.add(new Coil("L1", 0.0155)); // 15.5mH
+ sc.add(new Coil("L2", 47E-6)); // 47µH
+ sc.add(new Capacitor("C1", 100E-9)); // 100nF
+ sc.add(new Capacitor("C2", 100E-12)); // 100pF
+
+ sc.component("C1").setVoltage(5); // 5V
+
+ System.out.println(sc);
+ System.out.println(" Current/A: " + sc.getCurrent());
+ System.out.println(" Voltage/V: " + sc.totalVoltage());
+ System.out.println(" Power/W : " + sc.totalPower());
+ System.out.println(" Energy/J : " + sc.totalEnergy());
+
+ System.out.println("");
+ for (Component c : sc.getComponents()) {
+ System.out.println(c);
+ }
+
+ }
+}
+
